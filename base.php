05e6f8209f6a6208580ddfc7087e520520711541
Caio Andrade de Carvalho <caioandradedecarvalhokk@gmail.com>
2026-03-12 21:35:39 -0300

    First commit

---
+++ base.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Circuitos e PHP</title>
+    
+</head>
+<body>
+<?php
+
+?>
+<header>
+    <h1>Bem-vindo ao Trabalho Inicial</h1>
+</header>
+
+<nav>
+    <a href="#home">Home</a> |
+    <a href="#about">About</a> |
+    <a href="#contact">Contact</a>
+</nav>
+
+<main>
+    <p>This is the main content of the page.</p>
+</main>
+
+<footer>
+    <p>&copy; 2026 Nosso Projetinho</p>
+</footer>
+
+</body>
+</html>
