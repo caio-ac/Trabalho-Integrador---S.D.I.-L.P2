e9afa7230ec66debf4435223e6e466b3d0640c86
Caio Andrade de Carvalho <caioandradedecarvalhokk@gmail.com>
2026-03-19 13:04:58 -0300

    Corrigindo método get para método post

--- base.php
+++ base.php
@@ -21,7 +21,7 @@
 				<div class="row">
 					<div class="col-sm">
 						<div id="ResistorIdeal" class="card h-100">
-							<form>
+							<form action="base.php" method="post">
 								<div class="card-header">
 									<h5 class="card-title">Calculadora de Resistor ideal para LEDs em série</h5>
 								</div>
@@ -33,8 +33,7 @@
 												<div class="input-group-prepend">
 													<span class="input-group-text" id="inputGroupPrepend">@</span>
 												</div>
-												<input type="number" id="tensaoFonte" name="tensaoFonte"
-                                                       class="form-control" required><br>
+												<input type="number" id="tensaoFonte" name="tensaoFonte" class="form-control" ><br>
 											</div>
 										</div>
 										<div class="col-md-6 mb-4">
@@ -43,8 +42,7 @@
 												<div class="input-group-prepend">
 													<span class="input-group-text" id="inputGroupPrepend">@</span>
 												</div>
-												<input type="number" id="corrente" name="corrente" class="form-control"
-                                                       required><br>
+												<input type="number" id="corrente" name="corrente" class="form-control"><br>
 											</div>
 										</div>
 									</div>
@@ -55,8 +53,7 @@
 												<div class="input-group-prepend">
 													<span class="input-group-text" id="inputGroupPrepend">@</span>
 												</div>
-												<input type="number" id="tensaoLED" name="tensaoLED"
-                                                       class="form-control" required><br>
+												<input type="number" id="tensaoLED" name="tensaoLED" class="form-control"><br>
 											</div>
 										</div>
 										<div class="col-md-6 mb-3">
@@ -65,30 +62,28 @@
 												<div class="input-group-prepend">
 													<span class="input-group-text" id="inputGroupPrepend">@</span>
 												</div>
-												<input type="number" id="nLED" name="nLED" class="form-control"
-                                                       required><br>
+												<input type="number" id="nLED" name="nLED" class="form-control"><br>
 											</div>
 										</div>
 									</div>
 									<div class="form-row">
-										<br><input type="submit" value="Calcular Resistor" class="btn btn-primary"
-                                                   id="Enviar"><br>
+										<br><input type="submit" value="Calcular Resistor" class="btn btn-primary" id="Enviar"><br>
 									</div>
                                     <?php
-                                    if ($_GET["tensaoFonte"] != null && $_GET["corrente"] != null && $_GET["tensaoLED"] != null && $_GET["nLED"] != null) {
-                                        $tensaoFonte = $_GET["tensaoFonte"];
-                                        $corrente = $_GET["corrente"];
-                                        $tensaoLED = $_GET["tensaoLED"];
-                                        $nLED = $_GET["nLED"];
-                                        if ($tensaoLED * $nLED > $tensaoFonte) {
-                                            echo "<h2>A tensão total dos LEDs nunca pode ser maior que a tensão da fonte!</h2>";
-                                        } else {
-                                            $resistor = ($tensaoFonte - $tensaoLED * $nLED) / $corrente;
-                                            echo "<div class=\"alert alert-success\" role=\"alert\"><h4 class=\"alert-heading\">Cálculo realizado com sucesso!</h4><hr><p class=\"mb-0\">Resistor Ideal: $resistor ohms</p></div>";
-                                        }
-                                    } else {
-                                        echo "<div class=\"alert alert-warning alert-dismissible fade show\" role=\"alert\"><strong>É necessário preencher os campos de Tensão da Fonte, Corrente do LED, Tensão do LED e o número de LEDs em série</strong> </div>";
-                                    }
+	                                    if (isset($_POST["tensaoFonte"]) && isset($_POST["corrente"])  && isset($_POST["tensaoLED"]) && isset($_POST["nLED"]) ) {
+	                                        $tensaoFonte = $_POST["tensaoFonte"];
+	                                        $corrente = $_POST["corrente"];
+	                                        $tensaoLED = $_POST["tensaoLED"];
+	                                        $nLED = $_POST["nLED"];
+	                                        if ($tensaoLED * $nLED > $tensaoFonte) {
+	                                            echo "<h2>A tensão total dos LEDs nunca pode ser maior que a tensão da fonte!</h2>";
+	                                        } else {
+	                                            $resistor = ($tensaoFonte - $tensaoLED * $nLED) / $corrente;
+	                                            echo "<br><div class=\"alert alert-success\" role=\"alert\"><h4 class=\"alert-heading\">Cálculo realizado com sucesso!</h4><hr><p class=\"mb-0\">Resistor Ideal: $resistor ohms</p></div>";
+	                                        }
+	                                    } else {
+	                                        echo "<br><div class=\"alert alert-warning alert-dismissible fade show\" role=\"alert\"><strong>É necessário preencher os campos de Tensão da Fonte, Corrente do LED, Tensão do LED e o número de LEDs em série</strong> </div>";
+	                                    }
                                     ?>
 								</div>
 							</form>
