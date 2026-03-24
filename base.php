18ae9e80ab77aa10d5186fc6f954feb9cd653f0a
Caio Andrade de Carvalho <caioandradedecarvalhokk@gmail.com>
2026-03-24 07:27:31 -0300

    Ajustes do formulário e do roda-pé

--- base.php
+++ base.php
@@ -17,10 +17,10 @@
 		</header>
 
 		<main>
-			<div class="container">
+			<div class="container position-relative">
 				<div class="row">
 					<div class="col-sm">
-						<div id="ResistorIdeal" class="card h-100">
+						<div id="ResistorIdeal" class="card h-100 shadow-lg p-3 mb-5 bg-body-tertiary rounded">
 							<form action="base.php" method="post">
 								<div class="card-header">
 									<h5 class="card-title">Calculadora de Resistor ideal para LEDs em série</h5>
@@ -33,7 +33,7 @@
 												<div class="input-group-prepend">
 													<span class="input-group-text" id="inputGroupPrepend"><i>V<sub>fonte</sub></i></span>
 												</div>
-												<input type="number" id="tensaoFonte" name="tensaoFonte" class="form-control" ><br>
+												<input type="number" id="tensaoFonte" name="tensaoFonte" step="0.01" class="form-control" ><br>
 											</div>
 										</div>
 										<div class="col-md-6 mb-4">
@@ -42,7 +42,7 @@
 												<div class="input-group-prepend">
 													<span class="input-group-text" id="inputGroupPrepend"><i>I<sub>LED</sub></i></span>
 												</div>
-												<input type="number" id="corrente" name="corrente" class="form-control"><br>
+												<input type="number" id="corrente" name="corrente" step="0.01" class="form-control"><br>
 											</div>
 										</div>
 									</div>
@@ -53,7 +53,7 @@
 												<div class="input-group-prepend">
 													<span class="input-group-text" id="inputGroupPrepend"><i>V<sub>LED</sub></i></span>
 												</div>
-												<input type="number" id="tensaoLED" name="tensaoLED" class="form-control"><br>
+												<input type="number" id="tensaoLED" name="tensaoLED" step="0.01" class="form-control"><br>
 											</div>
 										</div>
 										<div class="col-md-6 mb-3">
@@ -62,12 +62,18 @@
 												<div class="input-group-prepend">
 													<span class="input-group-text" id="inputGroupPrepend"><i>N</i></span>
 												</div>
-												<input type="number" id="nLED" name="nLED" class="form-control"><br>
+												<input type="number" id="nLED" name="nLED" step="0.01" class="form-control"><br>
 											</div>
 										</div>
 									</div>
 									<div class="form-row">
-										<br><input type="submit" value="Calcular Resistor" class="btn btn-primary" id="Enviar"><br>
+										<br><input type="submit" value="Calcular Resistor" class="btn btn-primary col-md-12" id="Enviar"><br>
+										<svg xmlns="http://www.w3.org/2000/svg" class="d-none">
+											<symbol id="exclamation-triangle-fill" viewBox="0 0 16 16">
+												<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+											</symbol>
+										</svg>
+									
 									</div>
                                     <?php
 	                                    if (isset($_POST["tensaoFonte"]) && isset($_POST["corrente"])  && isset($_POST["tensaoLED"]) && isset($_POST["nLED"]) ) {
@@ -80,9 +86,11 @@
 	                                        } else {
 	                                            $resistor = ($tensaoFonte - $tensaoLED * $nLED) / $corrente;
 	                                            echo "<br><div class=\"alert alert-success\" role=\"alert\"><h4 class=\"alert-heading\">Cálculo realizado com sucesso!</h4><hr><p class=\"mb-0\">Resistor Ideal: $resistor ohms</p></div>";
+												
 	                                        }
 	                                    } else {
-	                                        echo "<br><div class=\"alert alert-warning alert-dismissible fade show\" role=\"alert\"><strong>É necessário preencher os campos de Tensão da Fonte, Corrente do LED, Tensão do LED e o número de LEDs em série</strong> </div>";
+	                                        echo "<br><br><div class=\"alert alert-warning d-flex align-items-center\" role=\"alert\"><svg class=\" p-2 flex-shrink-1 me-3\" role=\"img\" aria-label=\"Warning:\"><use xlink:href=\"#exclamation-triangle-fill\"/></svg>
+		                                    <div><strong>É necessário preencher os campos de Tensão da Fonte, Corrente do LED, Tensão do LED e o número de LEDs em série para obter o valor do resistor</strong></div></div>";
 	                                    }
                                     ?>
 								</div>
@@ -90,14 +98,16 @@
 						</div>
 					</div>
 					<div class="col-sm">
-
+						<div class="card h-100 shadow-lg p-3 mb-5 bg-body-tertiary rounded"></div>
 					</div>
 				</div>
 			</div>
 		</main>
-
-		<footer>
-			<p>&copy; 2026 Nosso Projetinho</p>
+		
+		<footer class="text-white text-center py-3">
+			<div class="text-center p-3" style="background-color: #007bff;">
+				© 2026 Equipe do trabalho do Antônio: Caio, Gabriela, Gilliard, Mateus, Miguel e Nicolas
+			</div>
 		</footer>
 
 	</body>
